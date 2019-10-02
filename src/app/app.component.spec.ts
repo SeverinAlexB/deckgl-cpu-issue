e6eced490ad9dc387e6ebe60a720dadb0cb97fea
Severin Bühler <severin.buehler@apgsga.ch>
2019-10-02 13:54:27 +0200

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,22 +10,5 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'deckgl-cpu-bug'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('deckgl-cpu-bug');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('deckgl-cpu-bug app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import {Deck} from '@deck.gl/core';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2VidWJ1IiwiYSI6ImNqZGxsdm85czA1N2Uyd3BjOWdsOGFnc3IifQ.2oxeuQlVi1zVoY_39M8GJA';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'deckgl-cpu-bug';
+export class AppComponent implements OnInit {
+  private deck: Deck;
+  private map: mapboxgl.Map;
+  private INITIAL_VIEW_STATE = {
+    latitude: 51.47,
+    longitude: 0.45,
+    zoom: 4,
+    bearing: 0,
+    pitch: 30
+  };
+
+
+  public initDeck() {
+    this.map = new mapboxgl.Map({
+      container: 'georeport-map',
+      style: 'mapbox://styles/mapbox/light-v9',
+      interactive: false,
+      center: [this.INITIAL_VIEW_STATE.longitude, this.INITIAL_VIEW_STATE.latitude],
+      zoom: this.INITIAL_VIEW_STATE.zoom,
+      pitch: this.INITIAL_VIEW_STATE.pitch
+    });
+    this.deck = new Deck({
+      canvas: 'georeport-deck-canvas',
+      initialViewState: this.INITIAL_VIEW_STATE,
+      controller: true,
+      width: '100%',
+      height: '100%',
+      onViewStateChange: ({viewState}) => {
+        this.map.jumpTo({
+          center: [viewState.longitude, viewState.latitude],
+          zoom: viewState.zoom,
+          bearing: viewState.bearing,
+          pitch: viewState.pitch
+        });
+      },
+      layers: [],
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.initDeck();
+  }
 }
